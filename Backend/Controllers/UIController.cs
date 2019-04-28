6c8c98d207bcc23106a9270faa294dc6b5c4bbc5
Andrii Vanikhin <a.vanikhin@bpmonline.com>
2019-04-28 11:43:13 +0300

    # prepare ui end point reqest

--- Backend/Controllers/UIController.cs
+++ Backend/Controllers/UIController.cs
@@ -3,18 +3,34 @@ using Checkpromise.UIData;
 using Checkpromise.Promises;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Checkpromise.Controllers
 {
     [EnableCors]
-    [Route("GetData")]
     [ApiController]
     public class UIController : ControllerBase
     {
-       [HttpPost]
+
+        private readonly IChartRepository _chartRepository;
+        private readonly IPromiseRepository _promiseRepository;
+
+        public UIController(IChartRepository chartRepository, IPromiseRepository promiseRepository)
+        {
+            _chartRepository = chartRepository;
+            _promiseRepository = promiseRepository;
+        }
+
+        [Route("GetData")]
+        [HttpGet]
         public ActionResult<Data> GetData()
         {
-            return new Data {};
+            var chartData = _chartRepository.GetAll().Select(c => new ChartData(c)).ToArray();
+            var promisesData = _promiseRepository.GetAll().Select(p => new PromiseData(p)).ToArray();
+            return new Data {
+                ChartData = chartData,
+                PromiseData = promisesData
+            };
         }
     }
 }
