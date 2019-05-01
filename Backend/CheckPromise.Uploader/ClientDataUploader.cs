a65ab0122cc45a349344d46f5ad407cdc8750906
A.Vanikhin <a.vanikhin@bpmonline.com>
2019-05-01 16:12:05 +0300

    #0 add CheckPromise.Uploader

---
+++ Backend/CheckPromise.Uploader/ClientDataUploader.cs
@@ -0,0 +1,18 @@
+﻿using Checkpromise.Provider;
+
+namespace CheckPromise.Uploader
+{
+	public class ClientDataUploader
+	{
+
+		private readonly IClientDataProvider dataProvider;
+
+		public ClientDataUploader(IClientDataProvider clientDataProvider) {
+			dataProvider = clientDataProvider;
+		}
+
+		public void UploadData() {
+			dataProvider.Push();
+		}
+	}
+}
