5c1432fd1ddad743ac04329858592c6b1a5044e3
A.Vanikhin <a.vanikhin@bpmonline.com>
2019-10-17 13:07:14 +0300

    #0 add indicator value

--- Backend/CheckPromise.Data/DataContext/CheckPromiseContext.cs
+++ Backend/CheckPromise.Data/DataContext/CheckPromiseContext.cs
@@ -9,7 +9,7 @@ namespace CheckPromise.Data.DataContext
 
         public DbSet<Promise> Promise { get; set; }
 		public DbSet<Indicator> Indicator { get; set; }
-        //public DbSet<IndicatorValue> IndicatorValues { get; set; }
+        public DbSet<IndicatorValue> IndicatorValue { get; set; }
 		public DbSet<GraphData> GraphData { get; set; }
 		public DbSet<MediaInfo> MediaInfo { get; set; }
 		//public DbSet<Currency> Currency { get; set; }
@@ -27,27 +27,32 @@ namespace CheckPromise.Data.DataContext
 			modelBuilder.Entity<Promise>().ToTable("Promise");
 
 			modelBuilder.Entity<Indicator>()
-                .Property(p => p.Id).ValueGeneratedOnAdd();
+                .Property(i => i.Id).ValueGeneratedOnAdd();
 			modelBuilder.Entity<Indicator>()
-				.Property(p => p.InvertArrow)
+				.Property(i => i.InvertArrow)
 				.IsRequired()
 				.HasDefaultValue(false);
+			modelBuilder.Entity<Indicator>().ToTable("Indicator");
 
-			/*modelBuilder.Entity<IndicatorValue>()
-                .Property(p => p.Id).ValueGeneratedOnAdd();
+			modelBuilder.Entity<IndicatorValue>()
+                .Property(iv => iv.Id).ValueGeneratedOnAdd();
             modelBuilder.Entity<IndicatorValue>()
-                .Property(p => p.Date)
+                .Property(iv => iv.Date)
                 .HasDefaultValueSql("getdate()");
-            modelBuilder.Entity<IndicatorValue>()
+			modelBuilder.Entity<IndicatorValue>()
+				.Property(iv => iv.Measure)
+				.HasDefaultValue(Measure.Empty);
+			/*modelBuilder.Entity<IndicatorValue>()
                 .HasOne(iv =>iv.Indicator)
-                .WithMany(i => i.Values)
+                .WithMany(i => i.Value)
                 .HasForeignKey(iv => iv.Indicator)
                 .HasConstraintName("ForeignKey_IndicatorValue_Indicator");*/
+			modelBuilder.Entity<IndicatorValue>().ToTable("IndicatorValue");
 
 			modelBuilder.Entity<GraphData>()
 				.Property(p => p.Id).ValueGeneratedOnAdd();
 			modelBuilder.Entity<GraphData>()
-				.Property(p => p.Date)
+				.Property(gd => gd.Date)
 				.IsRequired()
 				.HasDefaultValueSql("getdate()");
 			/*modelBuilder.Entity<GraphData>()
@@ -55,17 +60,18 @@ namespace CheckPromise.Data.DataContext
 				.WithMany(i => i.GraphData)
 				.HasForeignKey(gd => gd.Indicator)
 				.HasConstraintName("ForeignKey_GraphData_Indicator");*/
+			modelBuilder.Entity<GraphData>().ToTable("GraphData");
 
 			modelBuilder.Entity<MediaInfo>()
-				.Property(p => p.Id).ValueGeneratedOnAdd();
+				.Property(mi => mi.Id).ValueGeneratedOnAdd();
 			modelBuilder.Entity<MediaInfo>()
-				.Property(p => p.Date)
+				.Property(mi => mi.Date)
 				.IsRequired()
 				.HasDefaultValueSql("getdate()");
 			modelBuilder.Entity<MediaInfo>()
-				.Property(p => p.Caption).IsRequired();
+				.Property(mi => mi.Caption).IsRequired();
 			modelBuilder.Entity<MediaInfo>()
-				.Property(p => p.Source).IsRequired();
+				.Property(mi => mi.Source).IsRequired();
 			modelBuilder.Entity<MediaInfo>()
 				.HasOne(mi => mi.Indicator)
 				.WithMany(i => i.MediaInfoData);
--- Backend/CheckPromise.Data/Models/IndicatorValue.cs
+++ Backend/CheckPromise.Data/Models/IndicatorValue.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CheckPromise.Data.Models
 {
-    public class IndicatorValue
+	public enum Measure {
+		[Description("")]
+		Empty,
+		[Description("₴")]
+		UAH,
+		[Description("$")]
+		USD,
+		[Description("%")]
+		Parcent
+	}
+
+	public class IndicatorValue
     {
         public int Id { get; set; }
         public Indicator Indicator { get; set; }
         public DateTime Date { get; set; }
         public double Value { get; set; }
-    }
+
+		public Measure Measure { get; set; }
+
+		public string Quantity { get; set; }
+	}
 }
